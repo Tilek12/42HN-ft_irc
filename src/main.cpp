aa8c18fdab91d2ebc45f41f0b5c6542a3da1a176
tkubanyc <tkubanyc@student.42heilbronn.de>
2025-03-17 18:20:33 +0100

    check push once

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ main (void) {
 	std::cout << "task 5 done" << std::endl;
 	std::cout << "Hey beautiful Linda" << std::endl;
 	std::cout << "tilek work done " << std::endl;
+	std::cout << "check push to intra and lindagithub with one push" << std::endl;
 	return 0;
 }
 
