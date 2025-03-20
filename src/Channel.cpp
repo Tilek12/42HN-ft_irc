518a8759ec60ddcdbd6f6eeb9663d8603babbf29
Linda Lacsivy <lindalacsivy@outlook.de>
2025-03-20 20:15:26 +0100

    Channel.cpp done

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -37,3 +37,93 @@ Channel& Channel::operator=(const Channel& other)
 Channel::~Channel()
 {
 }
+
+std::string Channel::getName() const
+{
+	return _name;
+}
+
+std::string Channel::getTopic() const
+{
+	return _topic;
+}
+
+void Channel::setTopic(const std::string& topic)
+{
+	_topic = topic;
+}
+
+std::set<std::string> Channel::getUsers() const
+{
+	return _users;
+}
+
+void Channel::addUser(const std::string& user)
+{
+	_users.insert(user);
+}
+
+void Channel::removeUser(const std::string& user)
+{
+	_users.erase(user);
+}
+
+std::set<std::string> Channel::getOperators() const
+{
+	return _operators;
+}
+
+void Channel::addOperator(const std::string& oper)
+{
+	_operators.insert(oper);
+}
+
+void Channel::removeOperator(const std::string&oper)
+{
+	_operators.erase(oper);
+}
+
+std::set<std::string> Channel::getInvitedUsers() const
+{
+	return _invitedUsers;
+}
+
+void Channel::addInvitedUser(const std::string& user)
+{
+	_users.insert(user);
+}
+
+void Channel::removeInvitedUser(const std::string& user)
+{
+	_invitedUsers.erase(user);
+}
+
+bool Channel::getHasPassword() const
+{
+	return _hasPassword;
+}
+
+void Channel::setHasPassword(bool hasPassword)
+{
+	_hasPassword = hasPassword;
+}
+
+bool Channel::getIsInviteOnly() const
+{
+	return _isInviteOnly;
+}
+
+void Channel::setIsInviteOnly(bool isInviteOnly)
+{
+	_isInviteOnly = isInviteOnly;
+}
+
+size_t Channel::getUserLimit() const
+{
+	return _userLimit;
+}
+
+void Channel::setUserLimit(size_t userLimit)
+{
+	_userLimit = userLimit;
+}
