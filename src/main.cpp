66abcf9077f4533124d39b2d52edf6dc73cb8a29
Rustamjon Yusupov <ryusupov788@gmail.com>
2025-03-28 14:57:51 +0100

    pushing changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+// #include <iostream>
 
-main (void) {
-	std::cout << "ft_irc project" << std::endl;
-	std::cout << "Welcome" << std::endl;
-	std::cout << "Hey beautiful Linda" << std::endl;
-	std::cout << "testing issues feature again" << std::endl;
-	return 0;
-}
+// main (void) {
+// 	std::cout << "ft_irc project" << std::endl;
+// 	std::cout << "Welcome" << std::endl;
+// 	std::cout << "Hey beautiful Linda" << std::endl;
+// 	std::cout << "testing issues feature again" << std::endl;
+// 	return 0;
+// }
 
